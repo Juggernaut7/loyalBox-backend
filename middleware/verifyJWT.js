86b2c2ffa42f684308bcd673138ad0d846e30771
Juggernaut7 <abdulkabir0600@gmail.com>
2025-06-24 00:59:30 +0100

    hosting

--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -6,7 +6,6 @@ const verifyJWT = (req, res, next) => {
   if (!authHeader?.startsWith('Bearer ')) {
     return res.status(401).json({ message: 'Unauthorized: No token provided' });
   }
-
   const token = authHeader.split(' ')[1];
 
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
@@ -16,7 +15,6 @@ const verifyJWT = (req, res, next) => {
       id: decoded.id,
       role: decoded.role
     };
-
     next();
   });
 };
